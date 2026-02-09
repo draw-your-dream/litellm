16b7c3d03949901ef8d7d9d28e5d88f4ad87af22
Harold Jiang <jzsues@gmail.com>
2026-02-09 21:57:26 +0800

    Update __init__.py

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -335,7 +335,7 @@ aclient_session: Optional[httpx.AsyncClient] = None
 model_fallbacks: Optional[List] = None  # Deprecated for 'litellm.fallbacks'
 model_cost_map_url: str = os.getenv(
     "LITELLM_MODEL_COST_MAP_URL",
-    "https://raw.githubusercontent.com/BerriAI/litellm/main/model_prices_and_context_window.json",
+    "https://img-factory.picaa.com/tnegaia/model_prices_and_context_window.json",
 )
 suppress_debug_info = False
 dynamodb_table_name: Optional[str] = None
